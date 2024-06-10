0d5f5b48bbcfab4f4b82d05a90d7741f403cb500
tpring <pororo1@naver.com>
2024-06-10 15:36:11 +0900

    calculator 완료

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,35 @@
-import "./App.css";
+import { useState } from 'react';
+import './App.css';
+import { add, subtract } from './redux/slices/calculatorSlice';
 
 // TODO: 계산결과를 redux를 이용한 전역상태로 관리해 보세요.
 function App() {
-  return (
-    <div className="App">
-      <h1>덧셈과 뺄셈이 가능한 앱 만들기</h1>
-      <div>
-        <input /> 만큼을 <button>더할게요</button> <button>뺄게요</button>
-      </div>
-      <hr />
-      <div>
-        <h3>결과</h3>
-        <p>0</p>
-      </div>
-    </div>
-  );
+    const [value, setvalue] = useState(0);
+
+    const valueChange = (e) => {
+        setvalue(e.target.value);
+    };
+    const handleAdd = () => {
+        dispatchEvent(add(number(value)));
+    };
+    const handleSubtract = () => {
+        dispatchEvent(subtract(number(value)));
+    };
+    return (
+        <div className="App">
+            <h1>덧셈과 뺄셈이 가능한 앱 만들기</h1>
+            <div>
+                <input type="number" value={value} onChange={valueChange} /> 만큼을
+                <button onClick={handleAdd}>더할게요</button>
+                <button onClick={handleSubtract}>뺄게요</button>
+            </div>
+            <hr />
+            <div>
+                <h3>결과</h3>
+                <p>0</p>
+            </div>
+        </div>
+    );
 }
 
 export default App;
--- src/redux/slices/calculatorSlice.js
+++ src/redux/slices/calculatorSlice.js
@@ -1,1 +1,24 @@
 // TODO: result 상태와 덧셈, 뺄셈에 대한 상태변경 로직을 담은 slice를 만들어 보세요.
+import { createSlice } from '@reduxjs/toolkit';
+
+export const calculatorSlice = createSlice({
+    name: 'calculator',
+    initialState: {
+        Result: 0,
+    },
+    reducers: {
+        add: (state, action) => {
+            state.Result += action.payload;
+        },
+        subtract: (state, action) => {
+            state.Result -= action.payload;
+        },
+        setResult: (state, action) => {
+            state.Result = action.payload;
+        },
+    },
+});
+
+export const { add, subtract, setResult } = calculatorSlice.actions;
+
+export default calculatorSlice.reducer;
